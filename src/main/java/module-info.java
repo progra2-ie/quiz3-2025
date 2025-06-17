87406171113c4819816786c793af5ebc54d25eef
Alvaro Mena Monge <alvamen@hotmail.com>
2025-06-17 07:29:08 -0600

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module cr.ac.ucr.paraiso.progra2.quiz3.quiz32025 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens cr.ac.ucr.paraiso.progra2.quiz3.quiz32025 to javafx.fxml;
+    exports cr.ac.ucr.paraiso.progra2.quiz3.quiz32025;
+}
